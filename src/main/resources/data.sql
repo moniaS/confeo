c6961632bf07bddf11bb6d41ea5ed9ebf158cc0f
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-06-11 22:02:43 +0200

    small fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
 insert into address values (101, 'Lodz', 'Kwiatowa', 5);
-insert into address values (102, 'Kraków', 'Kwiatowa', 5);
+insert into address values (102, 'Kraków', 'Kwiatowa', 103);
+insert into address values (103, 'Warszawa', 'Łąkowa', 7);
+insert into address values (104, 'Kraków', 'Kwiatowa', 44);
+insert into address values (105, 'Gdańsk', 'Kwiatowa', 22);
+insert into address values (106, 'Poznać', 'Moniuszki', 33);
 insert into category values (1, 'TESTOWANIE');
 insert into category values (2, 'JAVASCRIPT');
 insert into category values (3, 'JAVA');
@@ -18,13 +22,13 @@ values(14, 'Konf4', 'Desc4', false, 10, 10, 101, 2, '2018-09-17', '2018-09-29', 
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
 values(15, 'Konf5', 'Desc5', false, 10, 10, 102, 2, '2018-09-12', '2018-09-12', 30, 'UPCOMING', 'LECTURE');
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
-values(16, 'Konf6', 'Desc6', false, 10, 10, 102, 2, '2018-07-18', '2018-07-19', 150, 'UPCOMING', 'LECTURE');
+values(16, 'Konf6', 'Desc6', false, 10, 10, 103, 2, '2018-07-18', '2018-07-19', 150, 'UPCOMING', 'LECTURE');
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
-values(17, 'Konf7', 'Desc7', false, 10, 10, 101, 1, '2018-06-02', '2018-06-05', 200, 'FINISHED', 'TRAINING');
+values(17, 'Konf7', 'Desc7', false, 10, 10, 104, 1, '2018-06-02', '2018-06-05', 200, 'FINISHED', 'TRAINING');
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
-values(18, 'Konf8', 'Desc8', false, 10, 10, 101, 1, '2018-05-04', '2018-05-08', 100, 'FINISHED', 'TRAINING');
+values(18, 'Konf8', 'Desc8', false, 10, 10, 105, 1, '2018-05-04', '2018-05-08', 100, 'FINISHED', 'TRAINING');
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
-values(19, 'Konf9', 'Desc9', false, 10, 10, 101, 2, '2018-06-09', '2018-06-10', 100, 'FINISHED', 'OTHER');
+values(19, 'Konf9', 'Desc9', false, 10, 10, 106, 2, '2018-06-09', '2018-06-10', 100, 'FINISHED', 'OTHER');
 insert into event (id, name, description, is_free, PRICE_PER_PARTICIPANT, PRICE_PER_PRELEGENT, address_id, category_id, start_date, end_date, max_participants, status, type)
 values(110, 'Konf10', 'Desc10', false, 10, 10, 102, 1, '2018-12-12', '2018-12-15', 100, 'UPCOMING', 'OTHER');
 
