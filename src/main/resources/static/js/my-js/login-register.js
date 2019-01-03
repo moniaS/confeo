f001213906faf7ef17465e0072a413dde79dd64e
Monika Stobieniecka <mstobieniecka1@gmail.com>
2019-01-03 16:44:14 +0100

    Added email validation on frontend using regex

--- src/main/resources/static/js/my-js/login-register.js
+++ src/main/resources/static/js/my-js/login-register.js
@@ -298,8 +298,22 @@ function validateEmail(isLoginForm) {
 
 function isEmailFormatCorrect(selector) {
     var element = $(selector);
-    //TODO ADD EMAIL VALIDATION
-    return true;
+    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(element.val()));
+    //     / .. /	All regular expressions start and end with forward slashes.
+    //     ^	    Matches the beginning of the string or line.
+    //     \w+	    Matches one or more word characters including the underscore. Equivalent to [A-Za-z0-9_].
+    //     [\.-]	\ Indicates that the next character is special and not to be interpreted literally.
+    //     .-       Matches character . or -.
+    //     ?	    Matches the previous character 0 or 1 time. Here previous character is [.-].
+    //     \w+	    Matches 1 or more word characters including the underscore. Equivalent to [A-Za-z0-9_].
+    //     *	    Matches the previous character 0 or more times.
+    //     ([.-]?\w+)*	Matches 0 or more occurrences of [.-]?\w+.
+    //     \w+([.-]?\w+)*	The sub-expression \w+([.-]?\w+)* is used to match the username in the email. It begins with at least one or more word characters including the underscore, equivalent to [A-Za-z0-9_]. , followed by . or - and . or - must follow by a word character (A-Za-z0-9_).
+    //     @	    It matches only @ character.
+    //     \w+([.-]?\w+)*	It matches the domain name with the same pattern of user name described above..
+    //     \.\w{2,3}	It matches a . followed by two or three word characters, e.g., .edu, .org, .com, .uk, .us, .co etc.
+    //     +	    The + sign specifies that the above sub-expression shall occur one or more times, e.g., .com, .co.us, .edu.uk etc.
+    //     $	    Matches the end of the string or line.
 }
 
 function isEmailValid(isLoginForm) {
